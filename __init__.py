18d5a8b132ac60673f6769524f3ee0512f6a9ce3
HotcakesSanderson <hotcakessanderson@gmail.com>
2019-07-15 10:02:41 -0500

    added class.function()

--- __init__.py
+++ __init__.py
@@ -7,9 +7,10 @@ class Rpirf(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+
     @intent_file_handler('rpirf.intent')
     def handle_rpirf(self, message):
-        self.tx_code(267580, 1, 173)
+        self.RFdevice.tx_code(267580, 1, 173)
         self.speak_dialog('rpirf')
 
 
