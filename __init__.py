4903927d4df18426ffe6e3cdeb29761f6bf96e7a
HotcakesSanderson <hotcakessanderson@gmail.com>
2019-07-15 11:32:37 -0500

    more intents

--- __init__.py
+++ __init__.py
@@ -8,13 +8,31 @@ class Rpirf(MycroftSkill):
         MycroftSkill.__init__(self)
 
 
-    @intent_file_handler('rpirf.intent')
-    def handle_rpirf(self, message):
+    @intent_file_handler('rpirfon.intent')
+    def handle_rpirfon(self, message):
         rfdevice = RFDevice(17) #default gpio pin
         rfdevice.enable_tx()
-        rfdevice.tx_code(267580, 1, 173) #code, protocol, pulse
+        rfdevice.tx_code(267571, 1, 172) #globes code, protocol, pulse
+        rfdevice.tx_code(267715, 1, 172) #stand code, protocol, pulse
+        rfdevice.cleanup()
+
+
+    @intent_file_handler('rpirfhomeon.intent')
+    def handle_rpirfhomeon(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267571, 1, 172) #globes code, protocol, pulse
+        rfdevice.tx_code(267715, 1, 172) #stand code, protocol, pulse
         rfdevice.cleanup()
         self.speak_dialog('rpirf')
+
+    @intent_file_handler('rpirfoff.intent')
+    def handle_rpirfoff(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267580, 1, 172) #globes code, protocol, pulse
+        rfdevice.tx_code(267723, 1, 172) #stand code, protocol, pulse
+        rfdevice.cleanup()
 
 
 def create_skill():
