89789e979a8c394e1c0acea3b74393b7f96312c2
hotcakessanderson <hotcakessanderson@gmail.com>
2019-09-23 17:32:29 -0500

    Added corner and shelf lights on/off.

--- __init__.py
+++ __init__.py
@@ -6,8 +6,7 @@ from mycroft import MycroftSkill, intent_file_handler
 class Rpirf(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-
+#turn on both lights
     @intent_file_handler('rpirfon.intent')
     def handle_rpirfon(self, message):
         rfdevice = RFDevice(17) #default gpio pin
@@ -15,8 +14,21 @@ class Rpirf(MycroftSkill):
         rfdevice.tx_code(267571, 1, 172) #globes code, protocol, pulse
         rfdevice.tx_code(267715, 1, 172) #stand code, protocol, pulse
         rfdevice.cleanup()
-
-
+#turn on the shelf lights
+    @intent_file_handler('rpirfonshelf.intent')
+    def handle_rpirfonshelf(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267571, 1, 172) #globes code, protocol, pulse
+        rfdevice.cleanup()
+#turn on the corner light
+    @intent_file_handler('rpirfoncorner.intent')
+    def handle_rpirfoncorner(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267715, 1, 172) #stand code, protocol, pulse
+        rfdevice.cleanup()
+#turn on both lights and respond welcome home
     @intent_file_handler('rpirfhomeon.intent')
     def handle_rpirfhomeon(self, message):
         rfdevice = RFDevice(17) #default gpio pin
@@ -25,7 +37,7 @@ class Rpirf(MycroftSkill):
         rfdevice.tx_code(267715, 1, 172) #stand code, protocol, pulse
         rfdevice.cleanup()
         self.speak_dialog('rpirf')
-
+#turn off both lights
     @intent_file_handler('rpirfoff.intent')
     def handle_rpirfoff(self, message):
         rfdevice = RFDevice(17) #default gpio pin
@@ -33,7 +45,20 @@ class Rpirf(MycroftSkill):
         rfdevice.tx_code(267580, 1, 172) #globes code, protocol, pulse
         rfdevice.tx_code(267724, 1, 173) #stand code, protocol, pulse
         rfdevice.cleanup()
-
+#turn off shelf light
+    @intent_file_handler('rpirfoffshelf.intent')
+    def handle_rpirfoffshelf(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267580, 1, 172) #globes code, protocol, pulse
+        rfdevice.cleanup()
+#turn off Corner light
+    @intent_file_handler('rpirfoffcorner.intent')
+    def handle_rpirfoffcorner(self, message):
+        rfdevice = RFDevice(17) #default gpio pin
+        rfdevice.enable_tx()
+        rfdevice.tx_code(267724, 1, 173) #stand code, protocol, pulse
+        rfdevice.cleanup()
 
 def create_skill():
     return Rpirf()
